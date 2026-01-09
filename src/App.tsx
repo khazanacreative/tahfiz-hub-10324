aeb452d58c9c701ea40f16e82ad4eb935b233b29
Mochamad Burhan Islami <107086262+khazanacreative@users.noreply.github.com>
2026-01-09 16:19:37 +0700

    bersih

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 
-// Landing & Auth
 import LandingMobile from "./pages/ustadz/LandingMobile";
 import AuthUstadz from "./pages/ustadz/AuthUstadz";
-
-// Ustadz Mobile Pages
 import DashboardMobile from "./pages/ustadz/DashboardMobile";
 import SetoranMobile from "./pages/ustadz/SetoranMobile";
 import DrillMobile from "./pages/ustadz/DrillMobile";
 import TahfidzMobile from "./pages/ustadz/TahfidzMobile";
 import TasmiMobile from "./pages/ustadz/TasmiMobile";
 import ProfilMobile from "./pages/ustadz/ProfilMobile";
-
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -29,31 +25,18 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/setoran" element={<SetoranHafalan />} />
-            <Route path="/drill" element={<DrillHafalan />} />
-            <Route path="/ujian-tasmi" element={<UjianTasmi />} />
-            <Route path="/ujian-tahfidz" element={<UjianTahfidz />} />
-            <Route path="/santri" element={<DataSantri />} />
-            <Route path="/halaqoh" element={<DataHalaqoh />} />
-            <Route path="/kelas" element={<DataKelas />} />
-            <Route path="/ustadz" element={<DataUstadz />} />
-            <Route path="/users" element={<DataUsers />} />
-            {/* Profil & Pengaturan */}
-            <Route path="/profil" element={<Profil />} />
-            <Route path="/pengaturan" element={<Pengaturan />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            {/* Landing & Auth */}
+            {/* Mobile Landing & Auth */}
             <Route path="/" element={<LandingMobile />} />
-            <Route path="/auth" element={<AuthUstadz />} />            
-            {/* Ustadz Mobile Routes */}
+            <Route path="/auth" element={<AuthUstadz />} />
+
+            {/* Ustadz Mobile */}
             <Route path="/ustadz" element={<DashboardMobile />} />
             <Route path="/ustadz/setoran" element={<SetoranMobile />} />
             <Route path="/ustadz/drill" element={<DrillMobile />} />
             <Route path="/ustadz/tahfidz" element={<TahfidzMobile />} />
             <Route path="/ustadz/tasmi" element={<TasmiMobile />} />
             <Route path="/ustadz/profil" element={<ProfilMobile />} />
+
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
